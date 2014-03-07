a3b32d30d01a436d490d2f9932f276e6b272959c
Marco Janssen <dev@marcojanssen.net>
2014-03-07 13:51:51 +0100

    Fixed vagrant bashrc

--- shell/init.sh
+++ shell/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo cp /vagrant/files/sources.list /etc/apt/sources.list
-sudo cp /vagrant/files/.bashrc dest=/home/vagrant/.bashrc
-sudo chown vagrant. /vagrant/files/.bashrc
-sudo cp /vagrant/files/.bashrc dest=/root/.bashrc
+sudo cp /vagrant/files/.bashrc /home/vagrant/.bashrc
+sudo chown vagrant. /home/vagrant/.bashrc
+sudo cp /vagrant/files/.bashrc /root/.bashrc
 sudo apt-get update
