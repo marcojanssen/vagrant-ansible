0d45ebc6b77186a23f28bd5246ba50f68c3e0bce
Marco Janssen <dev@marcojanssen.net>
2014-03-09 14:24:21 +0100

    Added user installation, added default ssh key vagrant

--- shell/init.sh
+++ shell/init.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
 sudo cp /vagrant/files/sources.list /etc/apt/sources.list
-sudo cp /vagrant/files/.bashrc /home/vagrant/.bashrc
-sudo chown vagrant. /home/vagrant/.bashrc
-sudo cp /vagrant/files/.bashrc /root/.bashrc
-sudo apt-get update
