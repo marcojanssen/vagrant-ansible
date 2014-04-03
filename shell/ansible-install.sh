0fc33baca0a15e3c1847d343b696926c858515f6
Marco Janssen <dev@marcojanssen.net>
2014-04-03 22:19:54 +0200

    Added symfony 2 galaxy module

--- shell/ansible-install.sh
+++ shell/ansible-install.sh
@@ -3,3 +3,4 @@ sudo apt-get update
 sudo apt-get install python-software-properties -y
 sudo apt-get install python-pip python-dev -y
 sudo pip install ansible
+sudo ansible-galaxy install servergrove.symfony2
